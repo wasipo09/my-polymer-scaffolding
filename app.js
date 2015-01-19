6a7d4a8ca751ff8a5aebceed79da91db502a0636
Wasin Pongmussaya <pwnlowed@icloud.com>
2015-01-19 10:45:29 +0000

    Made script + css as an external files

---
+++ app.js
@@ -0,0 +1,6 @@
+var template = document.querySelector('template[is="auto-binding"]');
+template.pages = [
+    {name: 'Single', hash: 'one'},
+    {name: 'page', hash: 'two'},
+    {name: 'app', hash: 'three'},
+     ];
